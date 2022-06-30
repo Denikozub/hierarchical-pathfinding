9a9c54098c2575df4bb50d0dd47ca535846aa820
Denis Kozub <denikozub@gmail.com>
2022-06-30 21:05:04 +0300

    refactor

--- types.h
+++ types.h
@@ -1,6 +1,12 @@
 #ifndef HIERARCHICAL_PATHFINDING_TYPES_H
 #define HIERARCHICAL_PATHFINDING_TYPES_H
 
-typedef unsigned long long uint64;
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdint>
+#include "path.h"
+
+typedef std::unordered_set<std::pair<uint64_t, Path>> adj_nodes;
+typedef std::unordered_map<uint64_t, adj_nodes> adj_list;
 
 #endif //HIERARCHICAL_PATHFINDING_TYPES_H
