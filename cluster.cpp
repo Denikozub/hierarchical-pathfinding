8337b435ef234376451156ab1f895acaaf419237
Denis Kozub <denikozub@gmail.com>
2022-07-05 12:33:11 +0300

    clusterize

--- cluster.cpp
+++ cluster.cpp
@@ -1,26 +1,32 @@
 #include "graph.h"
 
 void Graph::clusterize(double threshold) {
-
+    int cluster_no = 0;
+    for (const auto& cluster_nodes : find_clusters(threshold)) {
+        std::unordered_set<uint64_t> outer_nodes;
+        for (uint64_t cluster_node: cluster_nodes) {
+            nodes.at(cluster_node).set_cluster_no(cluster_no);
+            for (const auto& [neighbour, _] : node_neighbours_map.at(cluster_node)) {
+                if (!cluster_nodes.contains(neighbour)) {
+                    outer_nodes.insert(cluster_node);
+                    nodes.at(cluster_node).set_outer(true);
+                }
+            }
+        }
+        adj_list outer_neighbours_map;
+        for (uint64_t outer_node : outer_nodes) {
+            outer_neighbours_map[outer_node] = {};
+            for (uint64_t neighbour : outer_nodes) {
+                if (outer_node == neighbour) {
+                    continue;
+                }
+                Path path = find_path_astar(outer_node, neighbour, *this);
+                if (!path.empty()) {
+                    outer_neighbours_map[outer_node].insert({neighbour, path});
+                }
+            }
+        }
+        clusters[cluster_no] = outer_neighbours_map;
+        ++cluster_no;
+    }
 }
-
-//void Cluster::compute() {
-//    std::unordered_set<uint64_t> outer_nodes;
-//    for (uint64_t cluster_node: cluster_nodes) {
-//        for (const auto& [neighbour, _] : (*node_neighbours_map).at(cluster_node)) {
-//            if (!cluster_nodes.contains(neighbour)) {
-//                outer_nodes.insert(cluster_node);
-//                (*nodes).at(cluster_node).set_outer(true);
-//            }
-//        }
-//    }
-//    for (uint64_t outer_node : outer_nodes) {
-//        outer_neighbours_map[outer_node] = {};
-//        for (uint64_t neighbour : outer_nodes) {
-//            Path path = find_path_astar(outer_node, neighbour, nodes, node_neighbours_map);
-//            if (!path.empty()) {
-//                outer_neighbours_map[outer_node].insert({neighbour, path});
-//            }
-//        }
-//    }
-//}
--- graph.h
+++ graph.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <unordered_map>
+#include <unordered_set>
 #include "types.h"
 #include "path.h"
 
@@ -14,9 +15,10 @@ class Graph {
 private:
     node_map nodes = node_map{};
     adj_list node_neighbours_map = adj_list{};
-    std::unordered_map<uint64_t, adj_list> clusters {};
+    std::unordered_map<int, adj_list> clusters {};
     friend Graph from_graphml(const std::string&);
     friend Path find_path_astar(uint64_t, uint64_t, const Graph&, double);
+    std::unordered_set<std::unordered_set<uint64_t>> find_clusters(double);
 public:
     void clusterize(double threshold);
     const adj_nodes* get_neighbours(uint64_t) const;
