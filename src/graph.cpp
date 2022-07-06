40014df65083b96032140742476b6767e813bee3
Denis Kozub <denikozub@gmail.com>
2022-07-06 20:52:39 +0300

    fix cmake

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,6 +1,6 @@
 #include <cstring>
 #include "graph.h"
-#include "pugixml/pugixml.hpp"
+#include "pugixml.hpp"
 
 Graph from_graphml(const std::string& input_file) {
     pugi::xml_document document;
